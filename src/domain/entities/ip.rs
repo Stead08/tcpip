c08a981c785becc7d608604af86bb67960304b6a
Stead08 <62312581+Stead08@users.noreply.github.com.>
2024-05-01 11:53:12 +0900

    ipヘッダの実装、シリアライズ処理まで書いた

--- src/domain/entities/ip.rs
+++ src/domain/entities/ip.rs
@@ -1,32 +1,34 @@
 use crate::domain::value_objects::ipv4_address::Ipv4Addr;
+use crate::infrastructure::serialization::packet_serializer::Serialize;
+
 enum Ip {
     V4(String),
     V6(String),
 }
 
 enum Protocol {
-    ICMP = 1,
+    Icmp = 1,
     IP = 4,
-    TCP = 6,
-    UDP = 17,
+    Tcp = 6,
+    Udp = 17,
 }
 
 impl Protocol {
     pub fn from_u8(value: u8) -> Option<Protocol> {
         match value {
-            1 => Some(Protocol::ICMP),
+            1 => Some(Protocol::Icmp),
             4 => Some(Protocol::IP),
-            6 => Some(Protocol::TCP),
-            17 => Some(Protocol::UDP),
+            6 => Some(Protocol::Tcp),
+            17 => Some(Protocol::Udp),
             _ => None,
         }
     }
     pub fn to_u8(&self) -> u8 {
         match self {
-            Protocol::ICMP => 1,
+            Protocol::Icmp => 1,
             Protocol::IP => 4,
-            Protocol::TCP => 6,
-            Protocol::UDP => 17,
+            Protocol::Tcp => 6,
+            Protocol::Udp => 17,
         }
     }
 
@@ -87,5 +89,66 @@ impl Ipv4Header {
 impl Default for Ipv4Header {
     fn default() -> Self {
         Self::default()
+    }
+}
+
+impl Serialize for Ipv4Header {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buffer = Vec::new();
+        buffer.push((self.version << 4) | self.ihl);
+        buffer.push((self.dscp << 2) | self.ecn);
+        buffer.extend(&self.total_length.to_be_bytes());
+        buffer.extend(&self.identification.to_be_bytes());
+        buffer.push((self.flags << 5) | (self.fragment_offset >> 8) as u8);
+        buffer.push((self.fragment_offset & 0xFF) as u8);
+        buffer.push(self.ttl);
+        buffer.push(self.protocol.to_u8());
+        buffer.extend(&self.header_checksum.to_be_bytes());
+        buffer.extend(self.source.as_ref());
+        buffer.extend(self.destination.as_ref());
+        buffer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::domain::value_objects::ipv4_address::Ipv4Addr;
+    use crate::infrastructure::serialization::packet_serializer::Serialize;
+
+    #[test]
+    fn test_ipv4_header_to_bytes() {
+        let source = Ipv4Addr::new([192, 168, 0, 1]);
+        let destination = Ipv4Addr::new([192, 168, 0, 2]);
+        let header = Ipv4Header::new(source, destination, Protocol::Tcp);
+        let bytes = header.to_bytes();
+        assert_eq!(bytes[0], 0x45);
+        assert_eq!(bytes[1], 0);
+        assert_eq!(bytes[2], 0);
+        assert_eq!(bytes[3], 0);
+        assert_eq!(bytes[4], 0);
+        assert_eq!(bytes[5], 0);
+        assert_eq!(bytes[6], 0);
+        assert_eq!(bytes[7], 0);
+        assert_eq!(bytes[8], 0);
+        assert_eq!(bytes[9], 6);
+        assert_eq!(bytes[10], 0);
+        assert_eq!(bytes[11], 0);
+        assert_eq!(bytes[12], 192);
+        assert_eq!(bytes[13], 168);
+        assert_eq!(bytes[14], 0);
+        assert_eq!(bytes[15], 1);
+        assert_eq!(bytes[16], 192);
+        assert_eq!(bytes[17], 168);
+        assert_eq!(bytes[18], 0);
+        assert_eq!(bytes[19], 2);
+    }
+    #[test]
+    fn test_ipv4_header_size() {
+        let source = Ipv4Addr::new([192, 168, 0, 1]);
+        let destination = Ipv4Addr::new([192, 168, 0, 2]);
+        let header = Ipv4Header::new(source, destination, Protocol::Tcp);
+        let bytes = header.to_bytes();
+        assert_eq!(bytes.len(), 20);
     }
 }
--- src/domain/value_objects/mac_address.rs
+++ src/domain/value_objects/mac_address.rs
@@ -1,7 +1,7 @@
 const MAC_ADDR_SIZE: usize = 6;
 
 // MACアドレスの構造体, structの定義
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct MacAddr(pub [u8; MAC_ADDR_SIZE]);
 
 impl MacAddr {
@@ -21,6 +21,15 @@ impl MacAddr {
         MacAddr([0; MAC_ADDR_SIZE])
     }
     
+}
+
+
+impl std::fmt::Debug for MacAddr {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
+               self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5])
+    }
+
 }
 
 impl std::convert::AsRef<[u8]> for MacAddr {
